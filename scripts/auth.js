29167d21a7c6850429d646d262658c95efa04967
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2020-03-24 23:43:45 +0200

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,15 +1,35 @@
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
    if (user) {
-    db.collection('guides').get().then(snapshot => {
+    db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
       setupUI(user);
+    }).catch(err => {
+      console.log(err.message)
     });
    } else {
      setupUI();
      setupGuides([]);
    }
 });
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  db.collection('guides').add({
+    title: createForm['title'].value,
+    content: createForm['content'].value
+  }).then(() => {
+    // close the modal and reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message)
+  })
+})
 
 //signup
 const signupForm = document.querySelector("#signup-form");
