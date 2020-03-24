d60f057aeb144e790c1a7999bd4307ced8b91820
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2020-03-24 21:42:15 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,26 @@
-const guiseList = document.querySelector('.guides');
+const guideList = document.querySelector('.guides');
 
 //setup guides
 const setupGuides = (data) => {
 
+
+  if (data.length) {
   let html = '';
   data.forEach(doc => {
     const guide = doc.data();
     const li = `
-    <li>
-    <div class="cpllapsible-header grey lighten-4">${guide.title}</div>
-    <div class="collapsible-body white">${guide.content}</div>
-    </li>
+      <li>
+        <div class="collapsible-header grey lighten-4">${guide.title}</div>
+        <div class="collapsible-body white">${guide.content}</div>
+      </li>
     `;
     html += li
   });
+
+  guideList.innerHTML = html;
+} else {
+  guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>'
+}
 }
 
 // setup materialize components
@@ -21,6 +28,6 @@ document.addEventListener("DOMContentLoaded", function() {
   var modals = document.querySelectorAll(".modal");
   M.Modal.init(modals);
 
-  var items = documents.querySelectorAll(".collapsible");
+  var items = document.querySelectorAll(".collapsible");
   M.Collapsible.init(items);
-});
+})
